79fe505e36261e4946a21abe6e30cd2c8ecd13e0
DongYoung Kim <37283294+DONGYYYYY@users.noreply.github.com>
2018-11-22 09:30:00 +0900

    Update NetworkTask_checkReservationAble.java

--- app/src/main/java/connectDB/NetworkTask_checkReservationAble.java
+++ app/src/main/java/connectDB/NetworkTask_checkReservationAble.java
@@ -16,7 +16,7 @@ public class NetworkTask_checkReservationAble extends AsyncTask<Reservation, Voi
 
     @Override
     protected Integer doInBackground(Reservation... voids) {
-        String url = "http://203.253.255.113:9090/reservation/checkReservationAble";
+        String url = "http://개인db/reservation/checkReservationAble";
         String queryurl = url + "?petspital="+voids[0].getPetspital()+"&regdate="+voids[0].getRegdate();
         String bodyStr = "";
         int result;
